afe9a750818bb8ceaaa0d7404738e5096357b9a8
benroz3 <benroz35@gmail.com>
2023-09-08 11:52:30 +0300

    added a project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,7 @@
 import netflixImg from "../../public/assets/projects/netflix.png";
 import fiverrImg from "../../public/assets/projects/fiverr.png";
 import map from "../../public/assets/projects/map.png";
+import clothes from "../../public/assets/projects/clothes.png";
 import zombies from "../../public/assets/projects/zombieHordeGame.png";
 import nike from "../../public/assets/projects/nike.png";
 import admin from "../../public/assets/projects/admin.png";
@@ -31,26 +32,33 @@ export const projects = [
     link: "https://github.com/benroz3/travel-map-app",
   },
   {
-    title: "Spotify Music Rooms",
-    img: spotify,
-    type: "Fullstack",
-    tech: "React, TypeScript, MUI, Django, PostgreSQL",
-    link: "https://github.com/benroz3/spotify-music-controller-room",
-  },
-  // {
-  //   title: "Games Library",
-  //   img: gamesLibrary,
-  //   type: "Fullstack",
-  //   tech: "Vue.js, Bootstrap, Flask",
-  //   link: "https://github.com/benroz3/games-library",
-  // },
-  {
     title: "Online Coder",
     img: coder,
     type: "Fullstack",
     tech: "MongoDB, ExpressJS, React, NodeJS, Socket.io",
     link: "https://main--candid-beijinho-70d59d.netlify.app/",
   },
+  {
+    title: "Spotify Music Rooms",
+    img: spotify,
+    type: "Fullstack",
+    tech: "React, TypeScript, MUI, Django, PostgreSQL",
+    link: "https://github.com/benroz3/spotify-music-controller-room",
+  },
+  {
+    title: "Clothing Mobile App",
+    img: clothes,
+    type: "Front-end",
+    tech: "React Native",
+    link: "https://github.com/benroz3/clothing-app",
+  },
+  // {
+    //   title: "Games Library",
+    //   img: gamesLibrary,
+    //   type: "Fullstack",
+    //   tech: "Vue.js, Bootstrap, Flask",
+    //   link: "https://github.com/benroz3/games-library",
+    // },
   {
     title: "Zombie Horde Game",
     img: zombies,
